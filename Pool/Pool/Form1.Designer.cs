a1da9b41d616463832bd3975c4bda7a954702676
Ben Bogaert <benbogaert94@hotmail.com>
2018-03-14 14:13:55 +0100

    Database test4

--- Pool/Pool/Form1.Designer.cs
+++ Pool/Pool/Form1.Designer.cs
@@ -32,17 +32,15 @@
             this.BtnRegistreren = new System.Windows.Forms.Button();
             this.BtnAfsluiten = new System.Windows.Forms.Button();
             this.BtnSettings = new System.Windows.Forms.PictureBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             ((System.ComponentModel.ISupportInitialize)(this.BtnSettings)).BeginInit();
             this.SuspendLayout();
             // 
             // BtnLogin
             // 
             this.BtnLogin.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.BtnLogin.Location = new System.Drawing.Point(128, 39);
-            this.BtnLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnLogin.Location = new System.Drawing.Point(96, 32);
             this.BtnLogin.Name = "BtnLogin";
-            this.BtnLogin.Size = new System.Drawing.Size(127, 49);
+            this.BtnLogin.Size = new System.Drawing.Size(95, 40);
             this.BtnLogin.TabIndex = 0;
             this.BtnLogin.Text = "Inloggen";
             this.BtnLogin.UseVisualStyleBackColor = false;
@@ -50,10 +48,9 @@
             // 
             // BtnRegistreren
             // 
-            this.BtnRegistreren.Location = new System.Drawing.Point(128, 98);
-            this.BtnRegistreren.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnRegistreren.Location = new System.Drawing.Point(96, 80);
             this.BtnRegistreren.Name = "BtnRegistreren";
-            this.BtnRegistreren.Size = new System.Drawing.Size(127, 49);
+            this.BtnRegistreren.Size = new System.Drawing.Size(95, 40);
             this.BtnRegistreren.TabIndex = 1;
             this.BtnRegistreren.Text = "Registreren";
             this.BtnRegistreren.UseVisualStyleBackColor = true;
@@ -61,10 +58,9 @@
             // 
             // BtnAfsluiten
             // 
-            this.BtnAfsluiten.Location = new System.Drawing.Point(128, 158);
-            this.BtnAfsluiten.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnAfsluiten.Location = new System.Drawing.Point(96, 128);
             this.BtnAfsluiten.Name = "BtnAfsluiten";
-            this.BtnAfsluiten.Size = new System.Drawing.Size(127, 49);
+            this.BtnAfsluiten.Size = new System.Drawing.Size(95, 40);
             this.BtnAfsluiten.TabIndex = 2;
             this.BtnAfsluiten.Text = "Afsluiten";
             this.BtnAfsluiten.UseVisualStyleBackColor = true;
@@ -73,10 +69,9 @@
             // BtnSettings
             // 
             this.BtnSettings.Image = global::Pool.Properties.Resources.Gear;
-            this.BtnSettings.Location = new System.Drawing.Point(373, 0);
-            this.BtnSettings.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnSettings.Location = new System.Drawing.Point(280, 0);
             this.BtnSettings.Name = "BtnSettings";
-            this.BtnSettings.Size = new System.Drawing.Size(43, 39);
+            this.BtnSettings.Size = new System.Drawing.Size(32, 32);
             this.BtnSettings.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.BtnSettings.TabIndex = 3;
             this.BtnSettings.TabStop = false;
@@ -84,15 +79,14 @@
             // 
             // PooltafelMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(419, 229);
+            this.ClientSize = new System.Drawing.Size(314, 186);
             this.Controls.Add(this.BtnSettings);
             this.Controls.Add(this.BtnAfsluiten);
             this.Controls.Add(this.BtnRegistreren);
             this.Controls.Add(this.BtnLogin);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PooltafelMain";
             this.Text = "                 Pooltafel-Application";
             ((System.ComponentModel.ISupportInitialize)(this.BtnSettings)).EndInit();
@@ -106,7 +100,6 @@
         private System.Windows.Forms.Button BtnRegistreren;
         private System.Windows.Forms.Button BtnAfsluiten;
         private System.Windows.Forms.PictureBox BtnSettings;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
