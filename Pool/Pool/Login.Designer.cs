fe46bc334a4532ccc4cd267bf2c54e295e04e7e6
coenc <coenc@HP>
2018-03-14 12:05:17 +0100

    Login change

--- Pool/Pool/Login.Designer.cs
+++ Pool/Pool/Login.Designer.cs
@@ -29,29 +29,111 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.BtnWachtwoord = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.LbNaam = new System.Windows.Forms.Label();
+            this.LbWachtwoord = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(46, 55);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(81, 38);
+            this.button1.Size = new System.Drawing.Size(0, 0);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(101, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(178, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Location = new System.Drawing.Point(3, 171);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(87, 48);
+            this.BtnLogin.TabIndex = 3;
+            this.BtnLogin.Text = "Login";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            // 
+            // BtnWachtwoord
+            // 
+            this.BtnWachtwoord.Location = new System.Drawing.Point(110, 171);
+            this.BtnWachtwoord.Name = "BtnWachtwoord";
+            this.BtnWachtwoord.Size = new System.Drawing.Size(113, 48);
+            this.BtnWachtwoord.TabIndex = 4;
+            this.BtnWachtwoord.Text = "Wachtwoord vergeten";
+            this.BtnWachtwoord.UseVisualStyleBackColor = true;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(240, 171);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(87, 48);
+            this.BtnExit.TabIndex = 5;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            // 
+            // LbNaam
+            // 
+            this.LbNaam.AutoSize = true;
+            this.LbNaam.Location = new System.Drawing.Point(50, 33);
+            this.LbNaam.Name = "LbNaam";
+            this.LbNaam.Size = new System.Drawing.Size(45, 17);
+            this.LbNaam.TabIndex = 6;
+            this.LbNaam.Text = "Naam";
+            // 
+            // LbWachtwoord
+            // 
+            this.LbWachtwoord.AutoSize = true;
+            this.LbWachtwoord.Location = new System.Drawing.Point(9, 85);
+            this.LbWachtwoord.Name = "LbWachtwoord";
+            this.LbWachtwoord.Size = new System.Drawing.Size(86, 17);
+            this.LbWachtwoord.TabIndex = 7;
+            this.LbWachtwoord.Text = "Wachtwoord";
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.LbWachtwoord);
+            this.Controls.Add(this.LbNaam);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnWachtwoord);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Login";
+            this.Size = new System.Drawing.Size(330, 250);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button BtnLogin;
+        private System.Windows.Forms.Button BtnWachtwoord;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Label LbNaam;
+        private System.Windows.Forms.Label LbWachtwoord;
     }
 }
