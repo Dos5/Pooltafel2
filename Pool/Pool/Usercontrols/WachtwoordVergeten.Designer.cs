1bda9beeeedc35429aa1019141ec8f09ce8f7a6d
coenc <coenc@HP>
2018-03-29 09:50:44 +0200

    Spelling controle en ww vergeten netjes gemaakt.

--- Pool/Pool/Usercontrols/WachtwoordVergeten.Designer.cs
+++ Pool/Pool/Usercontrols/WachtwoordVergeten.Designer.cs
@@ -40,7 +40,7 @@
             // Lb_Wachtwoord
             // 
             this.Lb_Wachtwoord.AutoSize = true;
-            this.Lb_Wachtwoord.Location = new System.Drawing.Point(11, 108);
+            this.Lb_Wachtwoord.Location = new System.Drawing.Point(9, 98);
             this.Lb_Wachtwoord.Name = "Lb_Wachtwoord";
             this.Lb_Wachtwoord.Size = new System.Drawing.Size(86, 17);
             this.Lb_Wachtwoord.TabIndex = 15;
@@ -49,7 +49,7 @@
             // Lb_Naam
             // 
             this.Lb_Naam.AutoSize = true;
-            this.Lb_Naam.Location = new System.Drawing.Point(52, 57);
+            this.Lb_Naam.Location = new System.Drawing.Point(51, 47);
             this.Lb_Naam.Name = "Lb_Naam";
             this.Lb_Naam.Size = new System.Drawing.Size(45, 17);
             this.Lb_Naam.TabIndex = 14;
@@ -78,7 +78,7 @@
             // 
             // Tb_Wachtwoord
             // 
-            this.Tb_Wachtwoord.Location = new System.Drawing.Point(103, 106);
+            this.Tb_Wachtwoord.Location = new System.Drawing.Point(101, 96);
             this.Tb_Wachtwoord.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Tb_Wachtwoord.Name = "Tb_Wachtwoord";
             this.Tb_Wachtwoord.Size = new System.Drawing.Size(177, 22);
@@ -86,7 +86,7 @@
             // 
             // Tb_Naam
             // 
-            this.Tb_Naam.Location = new System.Drawing.Point(103, 57);
+            this.Tb_Naam.Location = new System.Drawing.Point(101, 47);
             this.Tb_Naam.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Tb_Naam.Name = "Tb_Naam";
             this.Tb_Naam.Size = new System.Drawing.Size(177, 22);
@@ -113,7 +113,7 @@
             this.Controls.Add(this.Tb_Wachtwoord);
             this.Controls.Add(this.Tb_Naam);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "WachtwoordVergeten";
             this.Size = new System.Drawing.Size(331, 250);
             this.ResumeLayout(false);
